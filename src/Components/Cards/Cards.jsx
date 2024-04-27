971697c5530b90dd3f057d6e3d368d53bcfa90d9
SAHED KHAN <ninja.sahedkhan@gmail.com>
2024-04-27 18:26:32 +0600

    6 card make done

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = () => {
             <h2 className=" text-3xl font-semibold mt-14 text-center">Properties</h2>
             <div className=" mt-5 grid grid-cols-1 lg:grid-cols-3 gap-10">
             {
-				crafts.map((craft,idx) => <Card key={idx} card={craft}></Card>)
+				crafts.map((craft) => <Card key={craft._id} card={craft}></Card>)
 			}
             
             </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -17,6 +17,7 @@ const routes = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>,
+            loader: () => fetch('http://localhost:5000/addCraft')
             
         },
         // {
